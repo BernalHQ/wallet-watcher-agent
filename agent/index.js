63850defcaa59b9f374379121bee9d14a0496d08
BernalHQ <bernalhq19@gmail.com>
2025-07-13 14:22:28 -0600

    add index.js

--- agent/index.js
+++ agent/index.js
@@ -5,8 +5,20 @@ const address = '0xcB1C1FdE09f811B294172696404e88E658659905';
 
 const addressOwn = '0xFC8Be90a35c5B61fcA9d09eC11d93bC5CAf5D640'; 
 
-const res = await scanWallet(address);
+async function run(adressWallet) {
 
+    console.log(`🔍 Escaneando tokens de la wallet: ${adressWallet}\n`);
 
+    const balances = await scanWallet(adressWallet);
+    const response = formatBalance(balances);
 
-console.log(formatBalance(res));
+    console.log(response);
+
+}
+
+if(process.argv[2]) {
+    const useAdress = process.argv[2];
+    run(useAdress);
+} else {
+    run(addressOwn);
+}
